b51aaf7e233cbfcc10dc9067f06e5c37477ab138
olive <olivealavine@gmail.com>
2022-06-28 17:30:52 -0500

    new form inputs update components state

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,18 +1,60 @@
+import React, { useState } from "react";
+
+const kDefaultFormState = {
+  title: "",
+  creator: "",
+};
+
 const NewBoardForm = () => {
+  const [formFields, setFormFields] = useState(kDefaultFormState);
+
+  const onTitleChange = (event) => {
+    console.log(event);
+    setFormFields({
+      ...formFields,
+      title: event.target.value,
+    });
+  };
+
+  const onCreatorChange = (event) => {
+    setFormFields({
+      ...formFields,
+      creator: event.target.value,
+    });
+  };
+  // this was setting state one at a time in seemingly separate objects
+  // const handleChange = (event) => {
+  //   const fieldName = event.target.name;
+  //   const fieldValue = event.target.value;
+
+  //   const newFormFields = { ...FormData, [fieldName]: fieldValue };
+  //   setFormFields(newFormFields);
+  // };
+
   return (
     <div className="NewBoardForm">
       <form>
         <h2>Create a New Board</h2>
         <div>
-          <label>Title</label>
-          <input name="title" type="text"></input>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formFields.title}
+            onChange={onTitleChange}
+          ></input>
         </div>
         <div>
-          <label>Creator</label>
-          <input name="creator" type="text"></input>
+          <label htmlFor="creator"> Creator</label>
+          <input
+            type="text"
+            name="creator"
+            value={formFields.creator}
+            onChange={onCreatorChange}
+          ></input>
         </div>
         <div>
-          <input type="submit" value="Add Board!"></input>
+          <input type="submit" value="Add board!"></input>
         </div>
       </form>
     </div>
