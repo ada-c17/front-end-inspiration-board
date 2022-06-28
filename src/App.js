c5de872a58afe49a60af361b9649ed49c5d79c6b
jeannie <jeanniezheng123@gmail.com>
2022-06-28 16:33:09 -0500

    Created components folder and files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from 'react-bootstrap'
 
 const App = () => {
   return (
     <div className="App">
       <button className="btn btn-primary">Test Me</button>
+      <Card style={{ width: '18rem' }}>
+        <Card.Title>Board 1</Card.Title>
+        <Card.Body>This is where you display your boards.</Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Title>Board 1</Card.Title>
+        <Card.Body>This is where you display your boards.</Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Title>Board 1</Card.Title>
+        <Card.Body>This is where you display your boards.</Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Title>Board 1</Card.Title>
+        <Card.Body>This is where you display your boards.</Card.Body>
+      </Card>
+
+
     </div>
   );
 };
