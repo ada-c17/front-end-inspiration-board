95a664b7807553cb534f7b1f5bc1e9f1e4ab99b3
olive <olivealavine@gmail.com>
2022-06-28 12:46:53 -0500

    adds input fields to NewBoardForm

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -3,7 +3,17 @@ const NewBoardForm = () => {
     <div className="NewBoardForm">
       <form>
         <h2>Create a New Board</h2>
-        <label>Title</label>
+        <div>
+          <label>Title</label>
+          <input name="title" type="text"></input>
+        </div>
+        <div>
+          <label>Creator</label>
+          <input name="creator" type="text"></input>
+        </div>
+        <div>
+          <input type="submit" value="Add Board!"></input>
+        </div>
       </form>
     </div>
   );
