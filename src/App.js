64d756987c6cf510c8665985a1255c2cf2637016
Kassidy Buslach <kjbuslach@gmail.com>
2022-06-28 14:02:22 -0700

    Added Components and began CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="App-title">Inspiration Board</h1>
+        <section className="routes">
+          <p className="create-board"> Create an Inspiration Board</p>
+          <p></p>
+          <p className="choose-board">Choose an inspiration board</p>
+        </section>
       </header>
+      <main></main>
     </div>
   );
 }
