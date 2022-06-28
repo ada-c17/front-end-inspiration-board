49db0019298051b0602802286ede779c30ef390c
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-06-28 15:05:39 -0500

    adds react and react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Test Me</button>
+    </div>
+  );
+};
+
+export default App;
