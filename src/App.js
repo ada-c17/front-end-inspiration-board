fce9f2ee28aa09e1c5b5751427f4bd2a172a0186
NinaPatrina <ninochca@gmail.com>
2022-06-28 18:44:37 -0700

    added react router and one test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        {/* <Home> */}
+        <h1>Inspiration from the OtterSpace</h1>
+        {/* <BoardList /> */}
+        {/* </Home> */}
+      </main>
+      <footer>
+        &copy; 2022 Ada Developers Academy ✨ Coders from the OtterSpace ✨
+        Doina ✨ Fena ✨ Marlyn ✨ Nina ✨
+      </footer>
     </div>
   );
 }
