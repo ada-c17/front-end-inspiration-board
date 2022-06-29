ff00a2b34152b5b62d924687f845b2db338855f6
Alexa Coffman <coffman.alexa@gmail.com>
2022-06-29 11:33:15 -0700

    added more css files, created boardlist component

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,5 +1,35 @@
-function BoardList() {
-  return <div></div>;
-}
+import React from "react";
+import Board from "./Board";
+import PropTypes from "prop-types";
+import "./boardlist.css";
 
-export default BoardList;
+const BoardList = (props) => {
+  const boardComponents = props.boards.map((board) => {
+    return (
+      <section>
+        <Board
+          id={board.id}
+          owner={board.owner}
+          title={board.title}
+          onUpdate={props.onUpdateBoard}
+        ></Board>
+      </section>
+    );
+  });
+  return (
+    <section>
+      <ul className="">{boardComponents}</ul>
+    </section>
+  );
+};
+
+BoardList.propTypes = {
+  boards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      owner: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ),
+  onUpdateBoard: PropTypes.func.isRequired,
+};
