1de5fca3ece340a65c35180457f1b8e0a69ac40b
Sabrina Huwang <sabrinahuwang1@gmail.com>
2022-06-29 11:27:09 -0700

    Added form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Board from "./components/Board";
+import BoardForm from "./components/BoardForm";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">Inspiration Board</header>
       <main>
         <div>
-          <Board />
+          <Board /> <BoardForm />
         </div>
       </main>
     </div>
---
+++ src/components/BoardForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BoardForm = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">Board Title</label> 
+        <input type="text" name="title"/>
+        <label htmlFor="title">Owner</label> 
+        <input type="text" name="title"/>
+      </form>
+    </div>
+  )
+  };
+
+export default BoardForm;
