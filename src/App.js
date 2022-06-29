d37378ff26adceeb6d8913ed66590e36dd2a90f7
Kiana <kianashui@gmail.com>
2022-06-29 11:49:28 -0700

    added boardform functionality

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
 import "./App.css";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Board from "./components/Board";
 import BoardForm from "./components/BoardForm";
 import BoardList from "./components/BoardList";
 import CardForm from "./components/CardForm";
 import CardList from "./components/CardList";
-import axios from "axios";
 
 function App() {
-<<<<<<< Updated upstream
   const [boards, setBoards] = useState([]);
-  const URL = 'https://peaceful-shelf-16152.herokuapp.com/';
+  const URL = "https://peaceful-shelf-16152.herokuapp.com/";
 
-  const getBoards= () => {
-    axios
-      .get(`${URL}/boards`)
-      .then((res) => {
-        const newBoards = res.data.map((board) => {
-          return {
-            id: board.board_id,
-            title: board.title,
-            owner: board.owner,
-          };
-        });
-        setBoards(newBoards);
-      })
-=======
+  const getBoards = () => {
+    axios.get(`${URL}/boards`).then((res) => {
+      const newBoards = res.data.map((board) => {
+        return {
+          id: board.board_id,
+          title: board.title,
+          owner: board.owner,
+        };
+      });
+      setBoards(newBoards);
+    });
+  };
+
   const addBoard = (boardData) => {
     axios
-      .post("localhost:5000/boards", boardData)
-      .then
-      //fetch boards
-      ()
->>>>>>> Stashed changes
+      .post(`${URL}/boards`, boardData)
+      .then(getBoards())
       .catch((err) => {
         console.log(err);
       });
   };
 
-<<<<<<< Updated upstream
   useEffect(getBoards, []);
-  
-=======
->>>>>>> Stashed changes
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
         <div>
           <h2>Boards</h2>
-          <BoardList boards={boards}/>
+          <BoardList boards={boards} />
         </div>
         <h2>Selected Board</h2>
         <Board />
