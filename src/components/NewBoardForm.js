595b402bdec4c6c42f5e853a7c4a7fb8db97ab29
GitHub4Gigi <g_maycock@outlook.com>
2022-06-28 17:53:26 -0700

    Trying to create a button that we can click on for show/hide board

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -4,13 +4,30 @@ import PropTypes from 'prop-types';
 
 
 const NewBoardForm = ({board_id, owner, title}) => {
+    const handleBoard = () => {
     const updatedEntry = {
         board_id: board_id, owner:owner, title:title
     };
-    NewBoardForm(updatedEntry)
+
+    let x = document.getElementsByClassName("board");
+    if (x.style.display === "none") {
+        x.style.display ="block";
+    } else {
+        x.style.display = "none";
+    }
+    handleBoard(updatedEntry)
+}
+
     return (
-        <div className="board"></div>
-    )
+        <div className="new-entry local">
+        <button className="board" onClick={handleBoard}>Show/Hide Board</button>
+        <h2 className="entry-name">{owner}</h2>
+        <section className="entry-bubble">
+        <p>{title}</p>
+        
+    </section>
+    </div>
+)
 };
 
 
