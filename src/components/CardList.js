2af249672f1257f74b48401815efc1cbb7416aeb
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 15:32:08 -0500

    Add JSX to render CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import Card from "./Card";
 
-const CardList = () => {};
+const CardList = (props) => {
+  const cardComponents = props.cardData.map((card) => {
+    return (
+      <Card
+        id={card.id}
+        message={card.message}
+        likeCount={card.likeCount}
+        boardId={card.boardId}
+        onAddLike={card.onAddLike}
+        onDeleteCard={card.onDeleteCard}
+      />
+    );
+  });
+
+  return (
+    <section>
+      <ul>{cardComponents}</ul>
+    </section>
+  );
+};
 
 CardList.propTypes = {
   cardData: PropTypes.arrayOf(PropTypes.object).isRequired,
