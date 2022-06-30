299399f18f4b74fd5159ed25753d6ca203eb8595
Danielle <dmbecker24@gmail.com>
2022-06-30 09:11:20 -0700

    Initialize Board and BoardList componenets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import BoardList from './components/BoardList';
-import CardList from './components/CardList';
-import NewBoardForm from './components/NewBoardForm';
-import NewCardForm from './components/NewCardForm';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BoardList from "./components/BoardList";
+import CardList from "./components/CardList";
+import NewBoardForm from "./components/NewBoardForm";
+import NewCardForm from "./components/NewCardForm";
+import "./App.css";
 
 function App() {
-  // STATE(boardsData: ListOfObjects, selectedBoard: id) 
+  // STATE(boardsData: ListOfObjects, selectedBoard: id)
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(null);
 
-  const URL = 'https://inspo-board-server.herokuapp.com'
+  const URL = "https://inspo-board-server.herokuapp.com";
 
   useEffect(() => {
     axios
-      .get(URL+'/boards')
+      .get(URL + "/boards")
       .then((response) => {
         setBoardsData(() => {
           return response.data.map((board) => {
@@ -23,25 +23,23 @@ function App() {
               title: board.title,
               boardId: board.board_id,
               owner: board.owner,
-              cards: board.cards
+              cards: board.cards,
             };
           });
         });
-        
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  
   return (
     <main className="App">
       <h1>Inspiration Boards</h1>
       <NewBoardForm />
       <NewCardForm />
-      <BoardList boardsData={boardsData}/>
-      <CardList selectedBoard={selectedBoard} boardsData={boardsData}/>
+      <BoardList boardsData={boardsData} />
+      <CardList selectedBoard={selectedBoard} boardsData={boardsData} />
     </main>
   );
 }
--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Board from './Board';
+import React from "react";
+import PropTypes from "prop-types";
+import Board from "./Board";
 
-const BoardList = () => {
+const BoardList = ({ boardsData }) => {
+  const boardComponents = boardsData.map((board) => {
+    return (
+      <Board
+        key={board.board_id}
+        title={board.title}
+        owner={board.owner}
+        cards={board.cards}
+      />
+    );
+  });
 
   return (
     <>
       <h2>Boards</h2>
-      <Board />
+      <section>
+        <ul>{boardComponents}</ul>
+      </section>
     </>
-    
-  )
+  );
+};
 
-}
+BoardList.propTypes = {
+  boardsData: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default BoardList;
