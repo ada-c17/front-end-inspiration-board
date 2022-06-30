bf14ead211e2accf002173603d6a87a0589eb0e9
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 09:30:52 -0500

    Add BoardList component and placeholder board data to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import NewBoardForm from "./components/NewBoardForm";
+import BoardList from "./components/BoardList";
 
 function App() {
+  // sample boards data to test BoardList
+  const boardSet = [
+    { title: "Memes", creator: "Michael Scott" },
+    { title: "Inspirational Quotes", creator: "Dwight Schrute" },
+    { title: "Romance Advice", creator: "Kelly Kapoor" },
+  ];
+
   return (
     <main className="App">
       <h1>Inspiration Board</h1>
+      <BoardList boards={boardSet} />
       <NewBoardForm />
     </main>
   );
