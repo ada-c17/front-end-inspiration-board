888f544463efe2524f061baf796d199819d834d0
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 15:51:19 -0500

    Add placeholders for async likeCard and deleteCard API calls

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import NewBoardForm from "./components/NewBoardForm";
 import BoardList from "./components/BoardList";
 
+// API call (patch) to update likeCount for a single card
+const likeCardAsync = (cardId) => {};
+
+// API call (delete) to delete card by id
+const deleteCardAsync = (cardId) => {};
+
 function App() {
   // sample boards data to test BoardList
-  const boardSet = [
-    { title: "Memes", creator: "Michael Scott" },
-    { title: "Inspirational Quotes", creator: "Dwight Schrute" },
-    { title: "Romance Advice", creator: "Kelly Kapoor" },
-  ];
+  // const boardSet = [
+  //   { title: "Memes", creator: "Michael Scott" },
+  //   { title: "Inspirational Quotes", creator: "Dwight Schrute" },
+  //   { title: "Romance Advice", creator: "Kelly Kapoor" },
+  // ];
 
   return (
     <main className="App">
