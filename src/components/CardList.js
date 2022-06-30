f76deec43e7edcaaac411e484e79dd1d62bc9164
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 15:21:46 -0500

    Add propTypes to CardList component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,4 +3,10 @@ import PropTypes from "prop-types";
 
 const CardList = () => {};
 
+CardList.propTypes = {
+  cardData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onAddLike: PropTypes.func.isRequired,
+  onDeleteCard: PropTypes.func.isRequired,
+};
+
 export default CardList;
