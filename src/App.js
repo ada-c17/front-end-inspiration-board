dd96dc0e3c846680351d55c23794b30299a61d82
Fena Dang <fenadang@gmail.com>
2022-06-29 18:40:58 -0700

    FD: Added in BoardForm.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
-import Home from "./components/Home.js";
+// import Home from "./components/Home.js";
+import BoardForm from "./components/BoardForm";
 
 function App() {
+  const makeNewBoard = (data) => {
+    console.log("Making New Board");
+  };
   return (
     <div className="App">
       <main>
-        <Home />
+        <h1>Inspiration from the OtterSpace</h1>
+        <BoardForm handleSubmission={makeNewBoard} />
       </main>
       <footer>
         &copy; 2022 Ada Developers Academy ✨ by Coders from the OtterSpace ✨
---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Board = (props) => {
+  return (
+    <main>
+      <h1>Board Name Goes Here</h1>
+      <h3>Owner Name Goes Here</h3>
+    </main>
+  );
+};
+
+export default Board;
