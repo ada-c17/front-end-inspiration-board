b64da3f0b672be3569510db32b344deeb13bd308
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 09:58:54 -0500

    Add useEffect (in progress) to re-render BoardList when boards prop is updated

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 const BoardList = (props) => {
+  // useEffect to re-render component anytime boards is updated
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/boards")
+      .then((response) => {
+        console.log("Successfully retrieved boards!");
+        console.log(response.data);
+      })
+      .catch(() => {
+        console.log("Error retrieving boards");
+      });
+  }, [props.boards]);
+
   // map function to return bullet list item with title of each board
   const boardTitles = props.boards.map((board) => {
     return <li>{board.title}</li>;
