c8515d4d39c03ced7dbd027cadc52ae356f2533f
Shayla Logan <shayla.logan709@gmail.com>
2022-06-29 17:25:06 -0700

    Coded App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import cardData from "./data/boardsandcards.json";
 
-function App() {
+const App = () => {
+  const [boardData, setUpdateBoard] = useState(cardData);
+  let [likedNum, setLikedNum] = useState(0);
+
+  const increaseLikeCount = (updatedCard) => {
+    const newCardData = boardData.map((card) => {
+      if (card.card_id === updatedCard.card_id) {
+        likedNum = card.likes_count;
+        increaseLikes();
+        return updatedCard;
+      } else {
+        return card;
+      }
+    });
+    setUpdateBoard(newCardData);
+  };
+  const increaseLikes = () => {
+    setLikedNum(parseInt((likedNum += 1)));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="App">
+      <header>
+        <h2>dream board</h2>
+        <h2>Board id: {cardData.board_id}</h2>
       </header>
+      <main>
+        <Board cards={cardData} onUpdateLiked={increaseLikeCount} />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
