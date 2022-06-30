8dae38f3dbf80e6d517f6c95295bb6e77bafec00
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-30 14:55:02 -0500

    Add state, propTypes to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Card = () => {};
+const Card = () => {
+  const [likeCount, setLikeCount] = useState(0);
+};
+
+Card.propTypes = {
+  id: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
+};
 
 export default Card;
