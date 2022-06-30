113e8fff26c5dd9f5d1f9310371de17b5d8f4e00
Poppy Meng <qim17@pitt.edu>
2022-06-29 23:01:27 -0700

    finish card component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
-
-const CardList = () => {
-
+import './CardList.css'
+const CardList = ({cards, onDeleteCallback, onLikeCallback}) => {
+  const getCardList=(cards)=>{
+    return cards.map((card) => {
+      return (
+        <Card
+        key={card.id}
+          board_id={card.board_id}
+          card_id={card.card_id}
+          message={card.title}
+          count_like={card.count_like}
+          onLikeCallback={onLikeCallback}
+          onDeleteCallback={onDeleteCallback}
+        />
+      );
+    });
+  }
   return (
     <>
-      <h2>Cards for {}</h2>
-      <Card />
+      <h2>Cards for {board_id}</h2>;
+      <ul> {getCardList(cards)}</ul>;
     </>
-  )
+    
 
-}
+  )
+};
+
+CardList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      card_id: PropTypes.number.isRequired,
+      board_id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      count_like: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  onLikeCallback: PropTypes.func.isRequired,
+  onDeleteCallback: PropTypes.func.isRequired,
+}; 
 
 export default CardList;
