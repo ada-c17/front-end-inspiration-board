f45d6162bfb7169a5b20cb0652b220b24eb3422a
Marlyn <marlynlopezz@gmail.com>
2022-06-30 02:07:08 -0700

    CardList function, render, and PropType added

--- src/CardList.js
+++ src/CardList.js
@@ -1,1 +1,28 @@
+import React from "react";
 import "./CardList.css";
+import Card from "./Card";
+import PropTypes from "prop-types";
+
+const CardList = ({ data }) => {
+  const cardComponents = data.map((card) => (
+    <Card
+      key={card.id}
+      id={card.id}
+      message={card.message}
+      likes_count={card.likes_count}
+    />
+  ));
+
+  return (
+    <div>
+      <h2 className="cardList"> List of cards</h2>
+      {cardComponents}
+    </div>
+  );
+};
+
+CardList.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default CardList;
