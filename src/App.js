8df01f30b3063ce51f17b4d6e66aabbe1787b92e
Doina Colun <doinacolun@macpro.local>
2022-06-30 11:27:12 -0700

    Added useEffect & getboardsfromapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import "./App.css";
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
-  const [data] = useState([
-    {
-      id: "1",
-      title: "Ada is great",
-      owner: "Nina",
-    },
-    {
-      id: "2",
-      title: "Life is easy",
-      owner: "N",
-    },
-  ]);
+  // const [data] = useState([
+  //   {
+  //     id: "1",
+  //     title: "Ada is great",
+  //     owner: "Nina",
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "Life is easy",
+  //     owner: "N",
+  //   },
+  // ]);
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    getBoardsFromAPI();
+  }, []);
+
+  const getBoardsFromAPI = () => {
+    axios.get("http://127.0.0.1:5000/boards")
+         .then((response) => {setBoards(response.data)})
+         .catch((error) => {console.log("Oh no!!!")});
+
+  }
+
   return (
     <div className="App">
       <h1>Inspiration from the OtterSpace</h1>
       <ul className="list">
-        {data.map((item) => (
+        {boards.map((item) => (
           <li key={item.id} className="list-item">
             <Link to={item.id}>{item.title}</Link>
           </li>
