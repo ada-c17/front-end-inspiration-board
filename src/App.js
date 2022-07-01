8b389d60418afda14f4212e681b0fbf6fda6ceb8
sana <sanakakadan22@yahoo.com>
2022-07-01 21:57:00 +0200

    add card component, create card component, delete button, likes button, load board from database

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import logo from "./logo.svg";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import Card from "./components/Card";
-import {Board} from "./components/Board";
+import { Board } from "./components/Board";
 import BoardDropDown from "./components/BoardDropDown";
-import CreateCard from "./components/CreateCard";
+import { CreateCard } from "./components/CreateCard";
 import CreateBoard from "./components/CreateBoard";
 
 function App() {
+  // const [boardData, setBoardData] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //   .get("http://127.0.0.1:5000/boards")
+  //   .then((response) =>
+  //     setBoardData(response.data)
+  //   )
+  // }, []);
+
   return (
     <div id="App">
       <header>
         <h1>Inspo Board</h1>
-
-        <Board title="Board" owner="Sana"></Board>
+        <Board id="1" title="Board" owner="Sana"></Board>
       </header>
     </div>
   );
