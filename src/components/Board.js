afe4f1b94be8ac79ad6406f25fbc99aa6c2780c2
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-07-07 09:48:42 -0500

    Add props to Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,4 +3,11 @@ import PropTypes from "prop-types";
 
 const Board = () => {};
 
+Board.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  creator: PropTypes.string.isRequired,
+  cards: PropTypes.arrayOf(PropTypes.object),
+};
+
 export default Board;
