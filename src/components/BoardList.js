558d697266208122b958a8b17013a651569d452b
Juli <juli.terapin@gmail.com>
2022-07-16 20:38:09 -0400

    Added BoardList component and CSS

---
+++ src/components/BoardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Board from './Board';
+import './BoardList.css';
+
+const BoardList = (props) => {
+  const boardItems = props.boardData.map((board) => {
+    return (
+      <Board
+        key={board.id}
+        id={board.id}
+        title={board.title}
+        displayBoardTitle={props.displayBoardTitle}
+      />
+    );
+  });
+
+  return <ul className="board-list">{boardItems}</ul>;
+};
+
+export default BoardList;
