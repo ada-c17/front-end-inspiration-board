558b1efea8b269a3d974e06f6e2e67840358bfca
Juli <juli.terapin@gmail.com>
2022-07-16 20:37:27 -0400

    Updated Board compontent to render title

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Board.css';
+
+const Board = (props) => {
+  const handleBoardClick = () => {
+    props.displayBoardTitle(props.title);
+    console.log('Working! Clicked!');
+  };
+
+  return (
+    <div onClick={handleBoardClick} className="Board">
+      {props.title}
+    </div>
+  );
+};
+
+export default Board;
