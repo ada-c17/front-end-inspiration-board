40e6ecb25740ac99d55b1a12fe6b6a8293495f0c
NinaPatrina <ninochca@gmail.com>
2022-07-16 22:08:39 -0700

    edit the board in progress

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BoardList.css";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const BoardList = (props) => {
   props.boards.sort((a, b) => a.title.localeCompare(b.title));
-  // const [showInput, setShowInput] = useState(false);
 
-  // const handleEditing = () => setShowInput(true);
+  const [showInput, setShowInput] = useState(false);
+  const handleEditing = () => setShowInput(true);
 
-  // const handleUpdatedDone = (event) => {
-  //   if (event.key === "Enter") {
-  //     setShowInput(false);
-  //   }
-  // };
+  const [boardData, setBoardData] = useState(props.boards);
+
+  let viewMode = {};
+  let editMode = {};
+
+  if (showInput) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
+  const handleFormInput = (event) => {
+    const inputElement = event.target;
+    const title = inputElement.title;
+    const value = inputElement.value;
+
+    const newBoardData = { ...boardData };
+    newBoardData[title] = value;
+    setBoardData(newBoardData);
+  };
+
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+    setShowInput(false);
+    props.editBoard(boardData.id, boardData.title);
+  };
 
   return (
     <div>
@@ -30,19 +51,32 @@ const BoardList = (props) => {
             >
               X
             </button>
-            {/* <input
-              type="text"
-              style={editMode}
+            {/* I can wrap it in view mode later */}
+            <div style={viewMode}>
+              <button id="edit-board" onClick={handleEditing}>
+                ✐
+              </button>
+            </div>
+            <form onSubmit={handleFormSubmission} style={editMode}>
+              <input
+                name="title"
+                type="text"
+                value={item.title}
+                onChange={handleFormInput}
+              />
+              <input type="submit" />
+            </form>
+            {/* 
+            <input
+              // type="text"
+              // style={editMode}
               className={textInput}
               value={title}
               onChange={(e) => {
-                props.setUpdate(e.target.value, id);
+                props.setUpdate(e.target.value, item.id);
               }}
-              onKeyDown={this.handleUpdatedDone}
-            />
-            <button id="edit-board" onClick={handleEditing}>
-              ✎ ✐
-            </button> */}
+              onKeyDown={handleUpdatedDone}
+            /> */}
           </li>
         ))}
       </ul>
