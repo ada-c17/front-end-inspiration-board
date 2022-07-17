a5c86d9ec63457fc5dd256e5e3b833d312529822
Juli <juli.terapin@gmail.com>
2022-07-16 20:39:07 -0400

    Updated App.css and App.js to test rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import BoardList from './components/BoardList';
+import boardData from './data/boardData.json';
 
 const App = () => {
-    // State: what file should it be? app
-    // can there be more than one state?
+  // State: what file should it be? app
+  // can there be more than one state?
 
-    // <section>
-    //   <Navigation>
-    //   </Navigation>
-    //   <Board>
-    //   </Board>
-    // </section>
-    return <div>Hello World</div>;
+  // <section>
+  //   <Navigation>
+  //   </Navigation>
+  //   <Board>
+  //   </Board>
+  // </section>
+
+  // what happens when Board is clicked
+  const [boardTitle, setBoardTitle] = useState('TBD');
+
+  const displayBoardTitle = (title) => {
+    setBoardTitle(title);
+  };
+  return (
+    <div>
+      <header>
+        <h1>INSPIRATION BOARD by Team Name</h1>
+      </header>
+      <main>
+        <h2>Selected Board: {boardTitle}</h2>
+        <BoardList
+          boardData={boardData}
+          displayBoardTitle={displayBoardTitle}
+        />
+      </main>
+    </div>
+  );
 };
 
 export default App;
