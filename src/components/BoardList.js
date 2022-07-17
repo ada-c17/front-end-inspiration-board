d8118e822ce209d268d2409fb9e7ef5599aa99f4
NinaPatrina <ninochca@gmail.com>
2022-07-16 23:49:58 -0700

    edit board fixed

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -2,84 +2,56 @@ import React, { useState } from "react";
 import "./BoardList.css";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import EditBoardForm from "./EditBoardForm";
 
+let edit_board = {};
 const BoardList = (props) => {
   props.boards.sort((a, b) => a.title.localeCompare(b.title));
 
-  const [showInput, setShowInput] = useState(false);
-  const handleEditing = () => setShowInput(true);
-
-  const [boardData, setBoardData] = useState(props.boards);
-
-  let viewMode = {};
-  let editMode = {};
-
-  if (showInput) {
-    viewMode.display = "none";
-  } else {
-    editMode.display = "none";
-  }
-
-  const handleFormInput = (event) => {
-    const inputElement = event.target;
-    const title = inputElement.title;
-    const value = inputElement.value;
-
-    const newBoardData = { ...boardData };
-    newBoardData[title] = value;
-    setBoardData(newBoardData);
+  const [showInput, setShowInput] = useState(true);
+  const handleEditing = (id) => {
+    setShowInput(false);
+    edit_board = props.boards.find((x) => x.id === id);
+    console.log(edit_board);
   };
 
-  const handleFormSubmission = (event) => {
-    event.preventDefault();
-    setShowInput(false);
-    props.editBoard(boardData.id, boardData.title);
+  const onEditSubmission = (board) => {
+    // I should change it to true only after response from the database
+    setShowInput(true);
+    props.editBoard(board.id, board.title);
   };
 
   return (
     <div>
-      <div>You have these spaces:</div>
-      <ul className="list">
-        {props.boards.map((item) => (
-          <li key={item.id} className="list-item">
-            <Link to={`${item.id}`} style={{ cursor: "pointer" }}>
-              {item.title}
-            </Link>
-            <button
-              id="delete-board"
-              onClick={() => props.deleteBoard(item.id)}
-            >
-              X
-            </button>
-            {/* I can wrap it in view mode later */}
-            <div style={viewMode}>
-              <button id="edit-board" onClick={handleEditing}>
-                ✐
-              </button>
-            </div>
-            <form onSubmit={handleFormSubmission} style={editMode}>
-              <input
-                name="title"
-                type="text"
-                value={item.title}
-                onChange={handleFormInput}
-              />
-              <input type="submit" />
-            </form>
-            {/* 
-            <input
-              // type="text"
-              // style={editMode}
-              className={textInput}
-              value={title}
-              onChange={(e) => {
-                props.setUpdate(e.target.value, item.id);
-              }}
-              onKeyDown={handleUpdatedDone}
-            /> */}
-          </li>
-        ))}
-      </ul>
+      {showInput ? (
+        <ul className="list">
+          {props.boards.map((item) => (
+            <li key={item.id} className="list-item">
+              <div>
+                <Link to={`${item.id}`} style={{ cursor: "pointer" }}>
+                  {item.title}
+                </Link>
+                <button
+                  id="delete-board"
+                  onClick={() => props.deleteBoard(item.id)}
+                >
+                  X
+                </button>
+                <span>
+                  <button
+                    id="edit-board"
+                    onClick={() => handleEditing(item.id)}
+                  >
+                    ✐
+                  </button>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <EditBoardForm board={edit_board} onEditSubmission={onEditSubmission} />
+      )}
     </div>
   );
 };
---
+++ src/components/EditBoardForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const EditBoardForm = (props) => {
+  const [boardData, setBoardData] = useState(props.board);
+  const [message, setMessage] = useState("");
+
+  const handleFormInput = (event) => {
+    const inputElement = event.target;
+    const name = inputElement.name;
+    const value = inputElement.value;
+
+    const newBoardData = { ...boardData };
+    newBoardData[name] = value;
+    setBoardData(newBoardData);
+  };
+
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+    if (boardData.title === "") {
+      setMessage("Please enter the title");
+    } else {
+      // there is a problem with the order of operations. change later to update database first
+      boardData.title = boardData.title
+        .toLowerCase()
+        .split(" ")
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+
+      setBoardData(boardData);
+      props.onEditSubmission(boardData);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmission}>
+        <input
+          name="title"
+          type="text"
+          className="title"
+          value={boardData.title}
+          onChange={handleFormInput}
+          placeholder={boardData.title}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>{message}</p>
+    </>
+  );
+};
+
+export default EditBoardForm;
