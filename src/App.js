5bb784d74802b8bc5031c60672f9bc6f67e8f829
Jodi Denney <iarrived@gmail.com>
2022-07-18 12:38:32 -0700

    added Delete Board Button and Back Button on Board. . Still need setBoardCallBack

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BoardList from "./components/BoardList";
 import Board from "./components/Board";
 
 function App() {
-  // const [display, setDisplay] = useState(null);
+  const [display, setDisplay] = useState(null);
 
-  // const toggleDisplay = () => {
-  //   console.log("If/Else func - Switch between BoardList & Board");
-  //   if (display) {
-  //     return <Board board_id="{display}" />;
-  //   } else {
-  //     return <BoardList />;
-  //   }
-  // };
-  //
-  // const changeDisplay = ({ setBoardCallback }) => {
-  //   console.log("This will update setDisplay");
-  //   // setDisplay(setBoardCallback) NOTE: setBoardCallback == board_id
-  // };
+  const toggleDisplay = () => {
+    console.log("If/Else func - Switch between BoardList & Board");
+    if (display) {
+      return <Board board_id="{display}" />;
+    } else {
+      return <BoardList />;
+    }
+  };
+
+  const changeDisplay = ({ setBoardCallback }) => {
+    console.log("This will update setDisplay");
+    // setDisplay(setBoardCallback) NOTE: setBoardCallback == board_id
+  };
 
   return (
     <div className="App">
@@ -29,7 +29,7 @@ function App() {
       {/* NOTE: DELETE LINES 31-34 ONCE changeDisplay() FUNC IS COMPLETE */}
       <main>
         <BoardList />
-        <Board board_id="1" />
+        <Board board_id="2" />
       </main>
       <footer>
         ©2022 C17 Otters students: Jodi D., Elaine S., Tori S., Andrea G.Z.
--- src/components/Board.js
+++ src/components/Board.js
@@ -23,11 +23,23 @@ const Board = ({ board_id }) => {
       .catch((error) => console.log("Didnt get board data", error));
   };
 
+  const deleteBoard = (id) => {
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_URL}/boards/${board_id}`)
+      .then(() => {
+        console.log("deleted board");
+        // setBoardCallBack(null)
+      })
+      .catch((error) => console.log(`Cannot delete board ${error}`));
+  };
+
   return (
     <div className="Board">
       <h1>{title}</h1>
       <h2>{owner}</h2>
       <Card />
+      <button onClick={() => deleteBoard(board_id)}>DELETE THIS BOARD</button>
+      <button onClick={() => console.log("setBaordCallBack(null)")}> 🔙</button>
     </div>
   );
 };
