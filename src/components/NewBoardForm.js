573acd6a907e791facffe03998a19ea4ebd9c6eb
jeannie <jeanniezheng123@gmail.com>
2022-07-18 15:56:50 -0500

    disable submit

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -4,115 +4,113 @@ import axios from "axios";
 import "./stylesheet/NewBoardForm.css";
 
 const AddNewBoardForm = ({ submitBoard }) => {
-  //state in charge for opening and closing modal
-  const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
+    //state in charge for opening and closing modal
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
 
-  //state in charge of updating form input data
-  const url = "https://back-end-inspiration-board.herokuapp.com/boards";
-  const [data, setData] = useState({
-    title: "",
-    owner: "",
-  });
 
-  //Form input stored in state
-  const handleInput = (e) => {
-    const newData = { ...data };
-    newData[e.target.id] = e.target.value;
-    setData(newData);
-    console.log(newData);
-  };
+    //state in charge of updating form input data
+    const url = "https://back-end-inspiration-board.herokuapp.com/boards";
+    const [data, setData] = useState({
+        title: "",
+        owner: "",
+    });
+    const isValid = data.title.length > 0 && data.owner.length > 0;
 
-  const submit = (e) => {
-    e.preventDefault();
-    // if (boardFormSubmit === undefined) {
-    //   return <h1>...loading</h1>;
-    // }
-    // boardFormSubmit();
-    // console.log("I've submitted");
-    const newData = {
-      title: data.title,
-      owner: data.owner,
+    //Form input stored in state
+    const handleInput = (e) => {
+        const newData = { ...data };
+        newData[e.target.id] = e.target.value;
+        setData(newData);
+        console.log(newData);
     };
-    submitBoard(newData);
-  };
-  //data in state gets posted to api
-  // const submitBoard = () => {
-  //   console.log("submit board ran");
-  //   axios
-  //     .post(url, {
-  //       title: data.title,
-  //       owner: data.owner,
-  //     })
-  //     .then((response) => {
-  //       // console.log(response.data);
-  //       console.log(getAllBoards);
-  //       getAllBoards();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
 
-  //   useEffect(() => {
-  //     getAllBoards();
-  //   }, [data]);
+    const submit = (e) => {
+        e.preventDefault();
+        const newData = {
+            title: data.title,
+            owner: data.owner,
+        };
+        submitBoard(newData);
+    };
+    //data in state gets posted to api
+    // const submitBoard = () => {
+    //   console.log("submit board ran");
+    //   axios
+    //     .post(url, {
+    //       title: data.title,
+    //       owner: data.owner,
+    //     })
+    //     .then((response) => {
+    //       // console.log(response.data);
+    //       console.log(getAllBoards);
+    //       getAllBoards();
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // };
 
-  return (
-    <section>
-      <button onClick={handleShow} type="button" className=" board-btn">
-        Add New Board{" "}
-      </button>
+    //   useEffect(() => {
+    //     getAllBoards();
+    //   }, [data]);
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton className="header">
-          <Modal.Title>Add Board</Modal.Title>
-        </Modal.Header>
+    return (
+        <section>
+            <button onClick={handleShow} type="button" className=" board-btn">
+                Add New Board{" "}
+            </button>
 
-        <Modal.Body>
-          <Form onSubmit={(e) => submit(e)}>
-            <Form.Group className="mb-3">
-              <Form.Control
-                onChange={(e) => handleInput(e)}
-                id="title"
-                value={data.title}
-                type="text"
-                placeholder="Title *"
-                required
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                onChange={(e) => handleInput(e)}
-                id="owner"
-                value={data.owner}
-                type="text"
-                placeholder="Owner *"
-                required
-              />
-            </Form.Group>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton className="header">
+                    <Modal.Title>Add Board</Modal.Title>
+                </Modal.Header>
 
-            <Button
-              className="btn btn-primary text-nowrap"
-              onClick={handleClose}
-              variant="success"
-              type="submit"
-              block
-            >
-              Submit
-            </Button>
-          </Form>
-        </Modal.Body>
+                <Modal.Body>
+                    <Form onSubmit={(e) => submit(e)}>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                onChange={(e) => handleInput(e)}
+                                id="title"
+                                value={data.title}
+                                type="text"
+                                placeholder="Title *"
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                onChange={(e) => handleInput(e)}
+                                id="owner"
+                                value={data.owner}
+                                type="text"
+                                placeholder="Owner *"
+                                required
+                            />
+                        </Form.Group>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </section>
-  );
+                        <Button
+                            className="btn btn-primary text-nowrap"
+                            onClick={handleClose}
+                            disabled={!isValid}
+                            variant="success"
+                            type="submit"
+                            block
+                        >
+                            Submit
+                        </Button>
+                    </Form>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </section>
+    );
 };
 
 export default AddNewBoardForm;
