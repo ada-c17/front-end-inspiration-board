1b2ae9c0ce0509d1f40746c814bd1e2a2c28dd2c
maria <camilatagleo@gmail.com>
2022-07-19 11:19:01 -0700

    hover to the buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-import NewBoardForm from './components/NewBoardForm.js';
-import Board from './components/Board.js';
-import CardList from './components/CardList.js';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import NewBoardForm from "./components/NewBoardForm.js";
+import Board from "./components/Board.js";
+import CardList from "./components/CardList.js";
 
 function App() {
-
   const [boardsData, setBoardsData] = useState([]);
 
-  const URL = 'https://insp-board-migrationmess.herokuapp.com/boards';
-  // when we first load 
+  const URL = "https://insp-board-migrationmess.herokuapp.com/boards";
+  // when we first load
   const [selectedBoard, setSelectedBoard] = useState({
-    title: '',
-    owner: '',
-    board_id: null
+    title: "",
+    owner: "",
+    board_id: null,
   });
-  
+
   const fetchBoards = () => {
     axios
       .get(URL)
@@ -26,7 +25,7 @@ function App() {
             key: board.board_id,
             board_id: board.board_id,
             title: board.title,
-            owner: board.owner
+            owner: board.owner,
           };
         });
         setBoardsData(newBoards);
@@ -38,12 +37,16 @@ function App() {
 
   useEffect(fetchBoards, []);
 
-  const selectBoard = (board) => { setSelectedBoard(board) };
+  const selectBoard = (board) => {
+    setSelectedBoard(board);
+  };
 
   const boardsElements = boardsData.map((board) => {
-    return (<li>
-      <Board board={board} onBoardSelect={selectBoard}></Board>
-    </li>)
+    return (
+      <li>
+        <Board board={board} onBoardSelect={selectBoard}></Board>
+      </li>
+    );
   });
 
   const createNewBoard = (boardInfo) => {
@@ -61,33 +64,52 @@ function App() {
   };
 
   const [isBoardFormVisible, setIsBoardFormVisible] = useState(true);
-  const toggleNewBoardForm = () => {setIsBoardFormVisible(!isBoardFormVisible)}
+  const toggleNewBoardForm = () => {
+    setIsBoardFormVisible(!isBoardFormVisible);
+  };
 
   return (
     <div className="page__container">
       <div className="content__container">
-      <h1>Inspiration Board</h1>
-          <section className="boards__container">
-            <section>
-              <h2>Boards</h2>
-              <ol className="boards__list">
-                {boardsElements}
-              </ol>
+        <h1>Inspiration Board</h1>
+        <section className="boards__container">
+          <section>
+            <h2>Boards</h2>
+            <ol className="boards__list">{boardsElements}</ol>
           </section>
           <section>
             <h2>Selected Board</h2>
-            <p className='boards__selected'>{selectedBoard.board_id ? `${selectedBoard.title} - ${selectedBoard.owner}` : 'Select a Board from the Board List!'}</p>
+            <p className="boards__selected">
+              {selectedBoard.board_id
+                ? `${selectedBoard.title} - ${selectedBoard.owner}`
+                : "Select a Board from the Board List!"}
+            </p>
           </section>
           <section className="new-board-form__container">
             <h2>Create a New Board</h2>
-            {isBoardFormVisible ? <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm> : ''}
-            <span onClick={toggleNewBoardForm} className='new-board-form__toggle-btn'>{isBoardFormVisible ? 'Hide New Board Form' : 'Show New Board Form'}</span>
+            {isBoardFormVisible ? (
+              <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm>
+            ) : (
+              ""
+            )}
+            <span
+              onClick={toggleNewBoardForm}
+              className="new-board-form__toggle-btn"
+            >
+              {isBoardFormVisible
+                ? "Hide New Board Form"
+                : "Show New Board Form"}
+            </span>
           </section>
-          </section>
-            {selectedBoard.board_id ? <CardList board={selectedBoard}></CardList> : ''}
+        </section>
+        {selectedBoard.board_id ? (
+          <CardList board={selectedBoard}></CardList>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default App;
