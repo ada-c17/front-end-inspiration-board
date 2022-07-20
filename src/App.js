b7e81f6a395060c3cf00b11484718db38988210f
Amber <ambertanaka@yahoo.com>
2022-07-20 11:51:45 -0700

    added boards to useEffect

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
       });
   };
 
-  useEffect(() => fetchBoards, []);
+  useEffect(() => fetchBoards, [boards]);
   useEffect(() => {
     if (selectedBoard === null) {
       return;
