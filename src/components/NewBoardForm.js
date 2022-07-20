d7bbf8c02350bd88bfb1849d059c8f0ad458fcbf
soridalac <lacsorida@gmail.com>
2022-07-20 13:13:11 -0700

    checking the length

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -24,7 +24,7 @@ const NewBoardForm = (props) => {
         name="title" 
         value={title} 
         onChange={handleTitleChange}
-        className={((title.length === 0) || (title.length > 40)) ? 'invalid-form-input' : 'invalid'}></input>
+        className={((title.length > 40) || (title.length === 0)) ? 'invalid-form-input' : ''}></input>
         <br></br>
         <label htmlFor="owner">Owner's Name</label>
         <input 
@@ -32,7 +32,7 @@ const NewBoardForm = (props) => {
         name="owner" 
         value={owner} 
         onChange={handleOwnerChange}
-        className={((owner.length === 0) || (owner.length > 40)) ? 'invalid-form-input' : ''}></input>
+        className={((owner.length > 40) || (owner.length === 0)) ? 'invalid-form-input' : ''}></input>
         <p>Preview: {title} - {owner}</p>
         <input 
         type="Submit"
