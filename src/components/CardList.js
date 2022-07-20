4ea008dac0f7b4c012cc75a0a3ab2fd27acf79f9
GitHub4Gigi <g_maycock@outlook.com>
2022-07-20 11:44:57 -0700

    pushing code

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import Card from "./Card";
+import axios from "axios";
 // import NewCardForm from "./NewCardForm";
 
 //props is an array of objects of card instances for a particular board
@@ -8,6 +9,42 @@ import Card from "./Card";
 const CardList = ({ cards, boardId }) => {
   //useEffect() -> when pages loads (boardId), makes an API call to boards/<boardId>/cards-> endpoint
   console.log(cards);
+  const [cardData, setCardData] = useState([]);
+  //const [isVisible, setVisibility] = useState(true);
+
+  useEffect(() => {
+  const getCard = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`).then((response) => {
+
+      const getCardSet = response.data.map((card) => {
+        const {cardId, message} = card;
+        return {cardId, message}
+      }) 
+      setCardData(getCardSet);
+    });
+  };
+}, []);
+  // const onDelete = (id) => {
+  //   axios
+  //     .delete(`${URL}/${id}`)
+  //     .then(() => {
+  //       getCard();
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+
+  // const addCard = (cardData) => {
+  //   axios.post(URL, cardData).then((response) => {
+  //     getCard();
+  //   });
+  // };
+
+// setCardData(cardData)
+//   const totalLikes = cardData.reduce((total, entry) => {
+//     return total + entry.liked;
+// }, 0);
   // console.log(boardId);
   const cardListArray = cards.map((card) => {
     return (
