e86429a9991ed9ba21c7e2acc0f3fd8a83e673d6
Shayla Logan <shayla.logan709@gmail.com>
2022-07-19 20:31:41 -0700

    Added Card Form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import BoardList from "./components/BoardList";
 import CardList from "./components/CardList";
 import BoardForm from "./components/BoardForm";
+import CardForm from "./components/CardForm";
 
 const kBaseUrl = "https://ssh-back-end-inspiration-board.herokuapp.com";
 
@@ -48,6 +49,8 @@ const App = () => {
 
   const showBoardForm =
     btnText === "Create New Dream" ? "hideBoard" : "showBoard";
+
+  const showCardForm = boardNum !== 0 ? "showCard" : "hideCard";
 
   const loadBoards = () => {
     getBoardData().then((boards) => {
@@ -126,6 +129,15 @@ const App = () => {
       .catch((err) => console.log(err));
   };
 
+  const addCardData = (addedCard) => {
+    const requestBody = { ...addedCard };
+
+    return axios
+      .post(`${kBaseUrl}/boards/${boardNum}/cards`, requestBody)
+      .then(() => loadCards(boardNum))
+      .catch((err) => console.log(err));
+  };
+
   const boardToggle = () => {
     if (btnText === "Create New Dream") {
       newText = "Hide Dream Form";
@@ -139,6 +151,17 @@ const App = () => {
     addBoardData(boardName)
       .then((newBoard) => {
         loadBoards((oldData) => [...oldData, newBoard]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleCardDataReady = (addedCard) => {
+    console.log(`Card was added: ${addedCard}`);
+    addCardData(addedCard)
+      .then((newCard) => {
+        loadCards((oldData) => [...oldData, newCard]);
       })
       .catch((err) => {
         console.log(err);
@@ -163,6 +186,10 @@ const App = () => {
           onDelete={onCardDelete}
           boardNum={boardNum}
         />
+        <CardForm
+          onAddCard={handleCardDataReady}
+          shouldHideCard={showCardForm}
+        ></CardForm>
       </main>
     </div>
   );
