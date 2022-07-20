994453f83362f38ccac72ecfaffe8922dd10fe62
Marlyn <marlynlopezz@gmail.com>
2022-07-19 20:08:44 -0700

    blurred background

--- src/App.js
+++ src/App.js
@@ -16,32 +16,34 @@ function App() {
   const onClickShowBoardlist = () => setShowResults(!showBoardList);
 
   return (
-    <div className="App">
-      {/* to use photo we import image from folder */}
-      <img
-        src={Otter}
-        alt={"otterspace"}
-        className="Otter"
-        onClick={play}
-      ></img>
-      <h1>Inspiration from the OtterSpace</h1>
-      {/* a button for the story/mulitverse toggle */}
-      <input
-        id="story-button"
-        type="submit"
-        value="Story / Multiverse"
-        onClick={onClickShowBoardlist}
-      />
-      {/* Toggle to display boardlist when clicked, otherwise it will show the story */}
-      {showBoardList ? <BoardList /> : <Story />}
-      {/* Code that links to page where we create a new world(board) */}
-      <Link to="/new">
-        <div id="add-new-space">Create alternate world</div>
-      </Link>
-      <footer id="main-page-footer">
-        &copy; 2022 Ada Developers Academy ✨ by Coders from the OtterSpace ✨
-        Doina ✨ Fena ✨ Marlyn ✨ Nina ✨
-      </footer>
+    <div>
+      <div className="App">
+        {/* to use photo we import image from folder */}
+        <img
+          src={Otter}
+          alt={"otterspace"}
+          className="Otter"
+          onClick={play}
+        ></img>
+        <h1>Inspiration from the OtterSpace</h1>
+        {/* a button for the story/mulitverse toggle */}
+        <input
+          id="story-button"
+          type="submit"
+          value="Story / Multiverse"
+          onClick={onClickShowBoardlist}
+        />
+        {/* Toggle to display boardlist when clicked, otherwise it will show the story */}
+        {showBoardList ? <BoardList /> : <Story />}
+        {/* Code that links to page where we create a new world(board) */}
+        <Link to="/new">
+          <div id="add-new-space">Create alternate world</div>
+        </Link>
+        <footer id="main-page-footer">
+          &copy; 2022 Ada Developers Academy ✨ by Coders from the OtterSpace ✨
+          Doina ✨ Fena ✨ Marlyn ✨ Nina ✨
+        </footer>
+      </div>
     </div>
   );
 }
