aa545bd4bd7f6a882827a73270946173141c92db
Saria Pastor <saria.pastor@protonmail.com>
2022-07-20 23:04:09 -0400

    refactors previous bug fix to use existing useEffect hook

--- src/App.js
+++ src/App.js
@@ -24,12 +24,6 @@ function App() {
     }
     /* Update number of cards so that React will see change */
     setCards([...cards]);
-    const updatedBoard = { ...activeBoard, cards: cards };
-    const newBoards = boards.map((board) => {
-      return activeBoard.board_id === board.board_id ? updatedBoard : board;
-    });
-    getBoards(newBoards);
-
     /* Delete card in the back end */
     axios
       .delete(`http://shiver-of-sharks.herokuapp.com/cards/${id}`)
@@ -71,7 +65,7 @@ function App() {
       .catch((error) => {
         console.log(<section>{error.response.data.message}</section>);
       });
-  }, []);
+  }, [cards]);
 
   const addBoardData = (newBoard) => {
     axios
@@ -95,13 +89,7 @@ function App() {
       })
       .then((response) => {
         console.log("success");
-        const newCards = [...cards, response.data.card];
-        const updatedBoard = { ...activeBoard, cards: newCards };
-        setCards(newCards);
-        const newBoards = boards.map((board) => {
-          return activeBoard.board_id === board.board_id ? updatedBoard : board;
-        });
-        getBoards(newBoards);
+        setCards([...cards, response.data.card]);
       })
       .catch((error) => {
         console.log(<section>{error.response.data.message}</section>);
