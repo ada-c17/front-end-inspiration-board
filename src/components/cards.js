06c88bafe074b9409cc66a3c18cb2688cb3ec4c6
Sujin Shin <susan.shin8@gmail.com>
2022-07-21 11:16:18 -0400

    sort buttons added

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,6 +7,61 @@ const Cards = (props) => {
   if (props.cards === []) {
     return null;
   }
+
+  function compareObjectLetters(object1, object2, key) {
+    const obj1 = object1[key].toUpperCase()
+    const obj2 = object2[key].toUpperCase()
+  
+    if (obj1 < obj2) {
+      return -1
+    }
+    if (obj1 > obj2) {
+      return 1
+    }
+    return 0
+  }
+
+  function compareObjectNums(object1, object2, key) {
+    const obj1 = object1[key]
+    const obj2 = object2[key]
+  
+    if (obj1 < obj2) {
+      return -1
+    }
+    if (obj1 > obj2) {
+      return 1
+    }
+    return 0
+  }
+
+  // console.log((props.cards.sort((a, b) => {
+  //   return compareObjectNums(a, b, 'likes_count')
+  //   })))
+
+  const idSort = () => {
+    let sorted = props.cards.sort((a, b) => {
+      return compareObjectNums(a, b, 'card_id')
+    })
+    let newArr = [...sorted]
+    props.setCards(newArr)
+  }
+
+  const alphabetSort = () => {
+    let sorted = (props.cards.sort((a, b) => {
+      return compareObjectLetters(a, b, 'message')
+    }))
+    let newArr = [...sorted]
+    props.setCards(newArr)
+  }
+
+  const likesSort = () => {
+    let sorted = (props.cards.sort((a, b) => {
+    return compareObjectNums(a, b, 'likes_count')
+    }))
+    let newArr = [...sorted]
+    props.setCards(newArr)
+  }
+
   const cardComponents = props.cards.map((card, index) => {
     return (
       <SingleCard
@@ -19,7 +74,19 @@ const Cards = (props) => {
     );
   });
   
-  return <section className="cards-display">{cardComponents}</section>;
+  return (
+    <section className="cards-display">{cardComponents}
+      <p>
+        <button onClick={idSort}>Sort By ID</button>
+      </p>
+      <p>
+        <button onClick={alphabetSort}>Sort by Alphabetical</button>
+      </p>
+      <p>
+        <button onClick={likesSort}>Sort by Number of Likes</button>
+      </p>
+    </section>
+  )
 };
 
 Cards.propTypes = {
@@ -34,3 +101,12 @@ Cards.propTypes = {
 };
 
 export default Cards;
+
+
+// create three buttons or drop down menu to choose how to sort cards
+// by id, by alaphbet(in message), by likes count
+
+// need to create an empty array 
+// and add sorted original array using spread operator (...)
+// (do the sort on props.cards)
+// use props.setCards to update state with the new array
