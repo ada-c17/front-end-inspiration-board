b74dd505345b7ae7c8472d83154381f311b064c9
angelica <djyl.ahn@gmail.com>
2022-07-21 11:40:43 -0700

    adds sorting to CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Card from "./Card";
 
 const CardList = (props) => {
-  const sortedCardData = props.cardData.sort((a, b) => {
-    return a.cardId - b.cardId;
-  });
-  const cardComponents = sortedCardData.map((card) => {
+  const [sortType, setSortType] = useState("id");
+  const [cardData, setCardData] = useState(props.cardData);
+
+  useEffect(() => {
+    const sortArray = (type) => {
+      const types = {
+        cardId: "cardId",
+        message: "message",
+        likesCount: "likesCount",
+      };
+      const sortProperty = types[type];
+      if (sortProperty === "message") {
+        const sorted = props.cardData.sort((a, b) =>
+          a[sortProperty].localeCompare(b[sortProperty])
+        );
+        setCardData(sorted);
+      } else {
+        const sorted = props.cardData.sort(
+          (a, b) => b[sortProperty] - a[sortProperty]
+        );
+        setCardData(sorted);
+      }
+    };
+    sortArray(sortType);
+  }, [sortType, props.cardData]);
+
+  const cardComponents = cardData.map((card) => {
     return (
       <Card
         id={card.cardId}
         message={card.message}
         likeCount={card.likesCount}
-        // boardId={card.boardId}
         onAddLike={props.onAddLike}
         onDeleteCard={props.onDeleteCard}
       />
@@ -21,6 +44,12 @@ const CardList = (props) => {
 
   return (
     <section>
+      <label>Sort By: </label>
+      <select onChange={(event) => setSortType(event.target.value)}>
+        <option value="cardId">New</option>
+        <option value="message">Alphabetical</option>
+        <option value="likesCount">Most Liked</option>
+      </select>
       <ul>{cardComponents}</ul>
     </section>
   );
